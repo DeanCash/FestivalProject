9ee227e22ebee3253c988a4de76aa941f020eaca
DeanCash <deans2005@hotmail.com>
2022-05-23 16:56:13 +0200

    commit

---
+++ data/assets/usernavbar.php
@@ -0,0 +1,9 @@
+<nav id="usernav">
+    <ul id="usernav-ul">
+        <li href="#">1</li>
+        <li href="#">2</li>
+        <li href="#">3</li>
+        <li href="#">4</li>
+        <li href="#">5</li>
+    </ul>
+</nav>
--- data/index.php
+++ data/index.php
@@ -1,12 +1,15 @@
+<?php require("assets/databaselink.php"); ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styling/indexstyle.css">
     <title>Festival - Home</title>
 </head>
 <body>
-    Lorem ipsum dolor sit amet.
+    <?php require("assets/usernavbar.php"); ?>
+    test
 </body>
 </html>
